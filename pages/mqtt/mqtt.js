fc1ccbe13faff9182c2b37ee81e6f9cdc51ddbbf
ws_ifchange_pc <shuang.wu@ifchange.com>
2018-11-15 14:22:52 +0800

    mqtt代码准备

--- pages/mqtt/mqtt.js
+++ pages/mqtt/mqtt.js
@@ -1,66 +1,211 @@
 // pages/mqtt/mqtt.js
+var {
+  Client,
+  Message
+} = require('../../utils/mqtt/paho-mqtt.js')
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
 
-  },
+    requestResult: '',
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    client: null
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  randomString: function (len) {
+
+    len = len || 32;
+
+    var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+
+    var maxPos = $chars.length;
+
+    var pwd = '';
+
+    for (let i = 0; i < len; i++) {
+
+      pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+
+    }
+
+    return pwd;
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  subscribe: function (filter, subscribeOptions) {
+
+    // 订阅
+
+    var client = this.data.client;
+
+    if (client && client.isConnected()) {
+
+      wx.showToast({
+
+        title: '订阅成功'
+
+      })
+
+      return client.subscribe(filter, subscribeOptions);
+
+    }
+
+    wx.showToast({
+
+      title: '订阅失败',
+
+      icon: 'success',
+
+      duration: 2000
+
+    })
 
   },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  publish: function (topic, message, qos = 0, retained = false) {
+
+    // 发布
+
+    var client = this.data.client;
+
+    if (client && client.isConnected()) {
+
+      var message = new Message(message);
+
+      message.destinationName = topic;
+
+      message.qos = qos;
+
+      message.retained = retained;
+
+      wx.showToast({
+
+        title: '发布成功'
+
+      })
+
+      return client.send(message);
+
+    }
+
+    wx.showToast({
+
+      title: '发送失败',
+
+      icon: 'success',
+
+      duration: 2000
+
+    })
 
   },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+
+
+  doSubscribe: function () {
+
+    console.log('doSubscribe');
+
+    this.subscribe('test/topic', {
+
+      qos: 1
+
+    })
 
   },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+
+
+  doPublish: function () {
+
+    console.log('doPublish');
+
+    this.publish('test/topic', 'Hello World', 1, false)
 
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+  doConnect: function () {
+
+    console.log('doConnect');
+
+    var that = this;
+
+    if (that.data.client && that.data.client.isConnected()) {
+
+      wx.showToast({
+
+        title: '不要重复连接'
+
+      })
+
+      return
+
+    }
+
+
+
+    var client = new Client('wss://xxx.xx.xx/mqtt', that.randomString());
+
+    client.connect({
+
+      useSSL: true,
+
+      cleanSession: true,
+
+      keepAliveInterval: 5,
+
+      onSuccess: function () {
+
+        wx.showToast({
+
+          title: '连接成功'
+
+        })
+
+        that.data.client = client
+
+        client.onMessageArrived = function (msg) {
+
+          if (typeof that.data.onMessageArrived === 'function') {
+
+            return that.data.onMessageArrived(msg)
+
+          }
+
+          wx.showModal({
+
+            title: msg.destinationName,
+
+            content: msg.payloadString
+
+          })
+
+        }
+
+        client.onConnectionLost = function (responseObject) {
+
+          if (typeof that.data.onConnectionLost === 'function') {
+
+            return that.data.onConnectionLost(responseObject)
+
+          }
+
+          if (responseObject.errorCode !== 0) {
+
+            console.log("onConnectionLost:" + responseObject.errorMessage);
+
+          }
+
+        }
+
+      }
+
+    });
 
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
-  }
+
 })
